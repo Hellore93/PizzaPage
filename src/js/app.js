5f7efd448fec9e8dc3e7b931fbcda6bc3fc6f2b9
Rafał Sztorc <sztorcu18@gmail.com>
2021-09-04 13:52:38 +0200

    adding a booking properties

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,24 @@
 import { settings, select, classNames } from './settings.js';
 import {Product} from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function(){
     const thisApp = this;
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
-    thisApp.activatePage(thisApp.pages[0].id);
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for( let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
 
     for(let link of thisApp.navLinks){
       link.addEventListener('click', function(event){
@@ -15,8 +26,10 @@ const app = {
         event.preventDefault();
         /*get page id from gref attribute */
         const id = clickedElement.getAttribute('href').replace('#', '');
-        /* run thisApp.activatePage with that id */
+        /*run thisApp.activatePage with that id */
         thisApp.activatePage(id);
+        /*change URL hash */
+        window.location.hash =  '#/' + id;
       });
     }
   },
@@ -61,10 +74,17 @@ const app = {
       });
   },
 
+  initBooking: function(){
+    const thisApp = this;
+    const bookingNew = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingNew);
+  },
+
   init: function(){
     const thisApp = this;
     thisApp.initPages();
     thisApp.initData();
+    thisApp.initBooking();
   },
 
   initCart: function(){
@@ -77,6 +97,7 @@ const app = {
       app.cart.add(event.detail.product);
     });
   }
+
 };
 app.init();
 app.initCart();
